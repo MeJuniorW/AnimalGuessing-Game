db2527355127075ad6adc5b71c22b68e1d857844
junior muriithi <murithiwagura@gmail.com>
2021-02-28 01:11:30 +0300

    Add project files.

---
+++ AnimalGuessing Game/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalGuessing_Game
+{
+    class Animal
+    {
+        public string name { get; set; }
+        public string location { get; set; }
+        public string covering { get; set; }
+
+        public Animal(string aName, string aLocation, string aCovering)
+        {
+            name = aName;
+            location = aLocation;
+            covering = aCovering;
+        }
+    }
+}
---
+++ AnimalGuessing Game/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalGuessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool playAgain = false;
+            
+
+            Animal giraffe = new Animal("giraffe", "savannah", "spots");
+            Animal polarbear = new Animal("polarbear", "arctic", "white fur");
+            Animal kingfisher = new Animal("kingfisher", "rainforest", "feathers");
+            Animal clownfish = new Animal("clownfish", "ocean", "scales");
+
+            List<Animal> animalsInZoo = new List<Animal>();
+            animalsInZoo.Add(clownfish);
+            animalsInZoo.Add(polarbear);
+            animalsInZoo.Add(kingfisher);
+            animalsInZoo.Add(giraffe);
+            
+            do
+            {
+                string[] listAnimal = new string[] { "giraffe", "polarbear", "kingfisher", "clownfish" };
+                Random r = new Random();
+                string randAnimal = listAnimal[r.Next(0, listAnimal.Length)];
+                foreach (var animal in animalsInZoo)
+                {
+                    if (animal.name == randAnimal)
+                    {
+                        Console.WriteLine($"Animal lives in {animal.location} and has {animal.covering}");
+                        Console.WriteLine("Guess the animal:");
+                        string guess = Console.ReadLine();
+                        if (guess == animal.name)
+                        {
+                            Console.WriteLine($"You are right! It is a {animal.name}");
+                        }
+                        else
+                            Console.WriteLine($"Nope! It's a {animal.name}");
+                    }
+                }
+                Console.WriteLine("Do you want to play again?(y/n)");
+                string s = Console.ReadLine();
+                if (s == "y")
+                    playAgain = true;
+                else
+                    break;
+            } while (playAgain == true);
+        }
+    }
+}
